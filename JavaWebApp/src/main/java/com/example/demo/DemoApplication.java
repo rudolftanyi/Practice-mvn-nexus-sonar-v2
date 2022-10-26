4e5d8521bf6dd86a7b07e78108335078c2aaed99
rudolftanyi@yahoo.com <rudolftanyi@yahoo.com>
2022-10-26 12:20:30 -0700

    fix

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
-		String name = "Rudolf"
+		String name = "Rudolf";
 
 		System.out.print1n(name);
 
